c468ee41f7d4cc3e20a096643614df88b28d7b4b
Omer <omerfarukatas96@gmail.com>
2022-06-06 10:09:01 +0300

    Completed - tests are broken

--- src/components/About.js
+++ src/components/About.js
@@ -2,7 +2,11 @@ import React from "react";
 import { image } from "../data/data";
 
 function About() {
-  return <div>About</div>;
+  return <div id="about">
+    <h2>About me</h2>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eligendi non quis exercitationem culpa</p>
+    <img src={image} alt="I made this"></img>
+  </div>;
 }
 
 export default About;
